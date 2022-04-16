5cc6c477be4e48f61b47752650c0050f9aad3134
van021417 <evanderpedrajeta@gmail.com>
2022-04-16 20:42:30 +0800

    First commit

---
+++ ModernLoginRegister-master/app/src/main/java/com/amier/modernloginregister/Log.java
@@ -0,0 +1,17 @@
+//package com.amier.modernloginregister;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.vishnusivadas.advanced_httpurlconnection.PutData;
+
+//public class Log extends AppCompatActivity {
+ //   TextInputEditText textInputEditTextemail, textInputEditTextpassword;
+  //  Button button;
